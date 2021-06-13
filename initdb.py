cf5eebf01bb841b991e21bf16f3543e58129a87f
Junette Lee <jlee63@gmail.com>
2021-06-13 13:15:37 -0500

    create heroku tables

--- initdb.py
+++ initdb.py
@@ -1,4 +1,9 @@
-from .app import db
+import click
+from flask.cli import with_appcontext
+from .app import db, Destinations
 
-# db.drop_all()
-db.create_all()
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+  # db.drop_all()
+  db.create_all()
